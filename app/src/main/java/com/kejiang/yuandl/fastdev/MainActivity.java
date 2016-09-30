a3d96996a29f477dd0f306bd0dd51b21991cbde1
linglongxin24 <linglongxin24@163.com>
2016-09-30 11:02:45 +0800

    添加了之前丢失的application配置设置

--- app/src/main/java/com/kejiang/yuandl/fastdev/MainActivity.java
+++ app/src/main/java/com/kejiang/yuandl/fastdev/MainActivity.java
@@ -1,16 +1,9 @@
 package com.kejiang.yuandl.fastdev;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import com.kejiang.yuandl.base.BaseActivity;
-import com.kejiang.yuandl.config.Config;
-import com.orhanobut.logger.Logger;
 
 import org.xutils.http.RequestParams;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends BaseActivity {
@@ -27,19 +20,13 @@ public class MainActivity extends BaseActivity {
     }
     @Override
     public void initData() {
-        RequestParams requestParams = new RequestParams("http://218.18.102.45:9988/SvrCardInfo.ashx?MethodName=RegisterSystem");
-        requestParams.setAsJsonContent(true);
-        requestParams.setBodyContent("{\"PhoneNO\":\"13609284854\",\"EmpPWD\":\"Gn9R\\/fkBHAo=\",\"Token\":\"BOMHo06T138pUcbY8P28Md2WL5exk63zfxWRUFrIrKxf5xsz7Q0Qtd5aEsNqUFmAVGfCsxHdaG1\\/AfkIxnGxVmY\\/xDmZNaNWb3ou7qi+S9a3L\\/EnGs9j\\/KQmy7kbSfOAL7S3TVHb8rUZPiTY9uNLdhqTnrfmhjxM8ozN\\/rdI5bI=\",\"TimeStamp\":\"1472713084\",\"SignString\":\"83a212f78e8ace39caac0da4f05d3a87\"}");
-        requestParams.addBodyParameter("ss","555");
-        requestParams.addBodyParameter("ss","555");
+        RequestParams requestParams = new RequestParams("http://10.58.187.40:8080/WebDemo/login?userName=zhangsan&passWord=45678");
         ajax(requestParams);
     }
 
     @Override
     public void netOnSuccess(Map<String, Object> data) {
         super.netOnSuccess(data);
-        List<Map<String, Object>> list = (List<Map<String, Object>>) data.get(Config.data);
-        Logger.d("list=" + list.toString());
     }
 
     @Override
